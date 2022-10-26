029cd1623f8d8fbf643153f31ab6d32493494111
yangniuhaojiang <542182536@qq.com>
2022-10-27 00:54:56 +0800

    feature:init graphql

---
+++ springboot2022/ingress/src/main/java/com/ynhj/ingress/configure/GateWayConfig.java
@@ -0,0 +1,24 @@
+package com.ynhj.ingress.configure;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class GateWayConfig {
+    @Bean
+    RouteLocator gateway(RouteLocatorBuilder rlb) {
+        return rlb.routes().route(rs -> rs.path("/proxy")
+                .filters(fs -> fs.setPath("/products")
+                        .addResponseHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+                        .retry(10)).uri("http://localhost:8080/")).build();
+    }
+
+    @Bean
+    WebClient webClient(WebClient.Builder builder) {
+        return builder.build();
+    }
+}
---
+++ springboot2022/ingress/src/main/java/com/ynhj/ingress/graphsqlController/GraphqlController.java
@@ -0,0 +1,25 @@
+package com.ynhj.ingress.graphsqlController;
+
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+@Controller
+public class GraphqlController {
+    private final WebClient http;
+
+    public GraphqlController(WebClient http) {
+        this.http = http;
+    }
+
+    @SchemaMapping(typeName = "Query", field = "products")
+    Flux<Product> products() {
+        return this.http.get().uri("http://localhost:8080/products")
+                .retrieve().bodyToFlux(Product.class);
+    }
+
+}
+
+record Product(Integer id, String name, String describe) {
+}
